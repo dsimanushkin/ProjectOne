1973f425f4886346b38ff102ec475566a2be6f5c
dsimanushkin <dsimanushkin74@gmail.com>
2021-01-15 22:52:39 -0500

    Fixed Moshi

--- app/src/main/java/com/devlab74/projectone/api/RetrofitBuilder.kt
+++ app/src/main/java/com/devlab74/projectone/api/RetrofitBuilder.kt
@@ -2,15 +2,18 @@ package com.devlab74.projectone.api
 
 import com.devlab74.projectone.util.Constants.Companion.BASE_URL
 import com.devlab74.projectone.util.LiveDataCallAdapterFactory
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.adapters.Rfc3339DateJsonAdapter
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
+import java.util.*
 
 object RetrofitBuilder {
 
     private val retrofitBuilder: Retrofit.Builder by lazy {
         Retrofit.Builder()
             .baseUrl(BASE_URL)
-            .addConverterFactory(MoshiConverterFactory.create())
+            .addConverterFactory(MoshiConverterFactory.create(Moshi.Builder().add(Date::class.java, Rfc3339DateJsonAdapter()).build()))
             .addCallAdapterFactory(LiveDataCallAdapterFactory())
     }
 
